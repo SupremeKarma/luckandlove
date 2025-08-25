0ec05057962a0940123925d435550aa3c2b5044d
Aman Kumar Mahato <amanmahato321@gmail.com>
2025-08-25 07:14:53 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/hooks/use-toast.ts
+++ src/hooks/use-toast.ts
@@ -7,7 +7,7 @@ import type {
   ToastActionElement,
   ToastProps,
 } from "@/components/ui/toast"
-import { toast } from "@/components/ui/sonner"
+import { toast as sonnerToast } from "sonner"
 
 const TOAST_LIMIT = 3
 const TOAST_REMOVE_DELAY = 1000000
@@ -141,6 +141,35 @@ function dispatch(action: Action) {
 
 type Toast = Omit<ToasterToast, "id">
 
+function toast(props: Toast) {
+  const id = genId()
+
+  const update = (props: ToasterToast) =>
+    dispatch({
+      type: "UPDATE_TOAST",
+      toast: { ...props, id },
+    })
+  const dismiss = () => dispatch({ type: "DISMISS_TOAST", toastId: id })
+
+  dispatch({
+    type: "ADD_TOAST",
+    toast: {
+      ...props,
+      id,
+      open: true,
+      onOpenChange: (open) => {
+        if (!open) dismiss()
+      },
+    },
+  })
+
+  return {
+    id: id,
+    dismiss,
+    update,
+  }
+}
+
 function useToast() {
   const [state, setState] = React.useState<State>(memoryState)
 
@@ -156,7 +185,7 @@ function useToast() {
 
   return {
     ...state,
-    toast,
+    toast: sonnerToast,
     dismiss: (toastId?: string) => dispatch({ type: "DISMISS_TOAST", toastId }),
   }
 }
